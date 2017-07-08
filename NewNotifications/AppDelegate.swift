6a6e6a6d20e5a589f2ae1d7afa5af4fca7c58ff7
Ricardo Herrera MAC <rherrerapetit@gmail.com>
2017-07-08 18:59:54 -0500

    adding actions to notifications.

--- NewNotifications/AppDelegate.swift
+++ NewNotifications/AppDelegate.swift
@@ -48,7 +48,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     private func configureUserNotifications() {
-        let category = UNNotificationCategory(identifier: "myNotificationCategory", actions: [], intentIdentifiers: [], options: [])
+        
+        let favAction = UNNotificationAction(identifier: "fistBump", title: "👊 Fist Bump", options: [])
+        let dismissAction = UNNotificationAction(identifier: "dismiss", title: "Dismiss", options: [])
+        
+        
+        
+        let category = UNNotificationCategory(identifier: "myNotificationCategory", actions: [favAction, dismissAction], intentIdentifiers: [], options: [])
         UNUserNotificationCenter.current().setNotificationCategories([category])
         
     }
@@ -59,5 +65,10 @@ extension AppDelegate: UNUserNotificationCenterDelegate {
     
     func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
         completionHandler(.alert)
+    }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        print("Response recieved for \(response.actionIdentifier)")
+        completionHandler() 
     }
 }
